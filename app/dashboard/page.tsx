640b8aaaf28059063851c571cdcad1d8c86f4f54
Lucas Wolfram <lucaswolfram23@web.de>
2024-11-07 20:43:25 +0100

    added clickable cards to the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+
+import { ClickableCard } from "@/components/ui/clickable-card"
 import { Header } from "@/components/ui/header"
 
 function Dasboard() {
   return (
     <div>
       <Header />
-      <h1 className="flex mt-5 justify-center">Hier entsteht unser dashboard</h1>
+      <div className="flex justify-center space-x-4 mt-4">
+
+        <ClickableCard
+          title="AZF Practice"
+          description="Click here to start practicing your AZF theory"
+          onClick={() => null}
+        />
+
+        <ClickableCard
+          title="BZF Practice"
+          description="Click here to start practicing your BZF theory"
+          onClick={() => null}
+        />
+      </div>
     </div>
   )
 }
