5199fa67f2178e4630ad8865e4980c4b87d22475
Lucas Wolfram <lucaswolfram23@web.de>
2024-11-08 15:33:18 +0100

    added a progress chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,14 @@
 
 import { ClickableCard } from "@/components/ui/clickable-card"
 import { Header } from "@/components/ui/header"
+import ProgressChartWithLabel, { ProgressChart } from "./components/ProgressChart"
 
 function Dasboard() {
   return (
     <div>
       <Header />
-      <div className="flex justify-center space-x-4 mt-4">
-
+      {/* AZF/BZF clickable cards */}
+      <div className="flex justify-center space-x-4 mt-4 mb-4">
         <ClickableCard
           title="AZF Practice"
           description="Click here to start practicing your AZF theory"
@@ -20,7 +21,14 @@ function Dasboard() {
           description="Click here to start practicing your BZF theory"
           onClick={() => null}
         />
+      </div>
 
+      {/* Progress chart */}
+      <div>
+        <ProgressChartWithLabel
+          title="Your Progress"
+          description="This graph shows your progress from the last 10 tests"
+        />
       </div>
     </div>
   )
