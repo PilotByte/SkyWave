a9d77796e03f968feee1b2e89369ba4be4151e83
Lucas Wolfram <lucaswolfram23@web.de>
2024-11-17 11:00:32 +0100

    added the question table for the admin area

--- app/admin/questions/page.tsx
+++ app/admin/questions/page.tsx
@@ -1,8 +1,26 @@
-'use client'
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { QuestionTable } from "./QuestionsTable";
 
 function Questions() {
+
   return (
-    null
+    <div className="space-y-3 h-full flex-col flex ">
+      <div>
+        <h3 className="font-medium text-lg">Sort</h3>
+        <p className="text-muted-foreground text-sm">
+          Get Questions from the Datapool and sort them to the respective
+          Sub-Subjects.
+        </p>
+      </div>
+      <Separator />
+      <QuestionTable
+        questions={[] as any}
+        columns={[]}
+        subSubjects={[]}
+      />
+    </div>
   );
 }
 
