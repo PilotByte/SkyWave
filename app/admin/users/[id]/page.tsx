899672b809edffcae5b052af8ad4b28218a9cc76
PxlLoewe <72106766+PxlLoewe@users.noreply.github.com>
2024-11-18 15:50:10 +0100

    added admin option for password reset

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,3 +1,25 @@
-export function ManageUserPage() {
-  return null;
+import { createClient } from '@/lib/supabase/client';
+import { ResetPasswordForm } from './ResetPassword';
+import { redirect } from 'next/navigation';
+
+async function ManageUserPage({ params }: { params: Record<string, string> }) {
+  const supabase = createClient();
+
+  const { data: user } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('id', params.id)
+    .single();
+
+  if (!user) redirect('/admin/users');
+
+  return (
+    <div>
+      {' '}
+      <h1 className="text-xl">Manage User</h1>
+      <ResetPasswordForm email={user.email} />
+    </div>
+  );
 }
+
+export default ManageUserPage;
