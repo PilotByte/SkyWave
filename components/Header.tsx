ee7a6a08dd39fc17f16beae8230deea16684f759
PxlLoewe <72106766+PxlLoewe@users.noreply.github.com>
2024-11-24 12:52:56 +0100

    added transparent logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,25 +2,28 @@
 
 import * as React from 'react';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Moon, Sun } from 'lucide-react';
-import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import { Button } from './ui/button';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import logo from '@/app/logo.png';
+import { ThemeToggle } from './ThemeToggle';
 
 const Header = () => {
-  const { setTheme } = useTheme();
   const path = usePathname();
 
   return (
     <div className="flex h-14 items-center justify-center bg-slate-800">
       {/* Navbar items */}
+      <div className="absolute left-10">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={50}
+          height={50}
+          className="rounded-full bg-slate-300 p-0"
+        />
+      </div>
       <div className="flex justify-center w-full md:w-auto">
         <Tabs
           defaultValue={(function () {
@@ -80,26 +83,7 @@ const Header = () => {
 
       {/*Darkmode toggle */}
       <div className="absolute right-10 flex justify-center gap-3">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme('light')}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('dark')}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('system')}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <ThemeToggle />
         <Button variant="outline" className="font-bold text-red-500">
           <Link href="/logout">Logout</Link>
         </Button>
