8a48dcb3ec0c480b173ffec26e03fa70de7ef1d2
PxlLoewe <72106766+PxlLoewe@users.noreply.github.com>
2024-12-07 23:01:10 +0100

    added pilotybte as safe domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['storage.ko-fi.com', 'pgyilvoitcbzmpeiebvf.supabase.co'],
+    domains: ['pilotbyte.de', 'pgyilvoitcbzmpeiebvf.supabase.co'],
   },
   eslint: {
     ignoreDuringBuilds: true,
